888bf2e64ff49670d68d4c6cf6637278c7c99fdf
Ryan Lewis <accounts+github@ryandlewis.dev>
2022-09-09 13:00:16 -0400

    Updated launch files

---
+++ launch/basestation.launch.py
@@ -0,0 +1,20 @@
+import os
+from launch import LaunchDescription
+from launch.actions import (
+    DeclareLaunchArgument,
+    ExecuteProcess,
+    IncludeLaunchDescription,
+)
+from launch.conditions import IfCondition, UnlessCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, LaunchConfiguration, PythonExpression, FindExecutable
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    # Create the launch description and populate
+    ld = LaunchDescription()
+    package_name = "mcity_proxy"
+    pkg_share = FindPackageShare(package=package_name).find(package_name)
